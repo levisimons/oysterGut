3cb491d20f93d02ccc3c087d64ad983d71ca82f8
Ariel Levi Simons <levisimons@gmail.com>
2017-06-17 11:11:38 -0700

    Update demultiplex.py

--- demultiplex.py
+++ demultiplex.py
@@ -8,5 +8,7 @@ leftRead = gzip.open('Undetermined_S0_L001_R1_001.fastq.gz','r')
 rightRead = gzip.open('Undetermined_S0_L001_R2_001.fastq.gz','r')
 rightIndex = gzip.open('Undetermined_S0_L001_I2_001.fastq.gz','r')
 for leftLine, rightLine, indexLine in izip(leftRead, rightRead, rightIndex):
-    if lineNum%4 == 2: print (leftLine.rstrip(), rightLine.rstrip(), indexLine.rstrip())
+    if lineNum%4 == 2:
+        leftIndex=leftLine[4:9]
+        print (leftIndex.rstrip(), leftLine.rstrip(), rightLine.rstrip(), indexLine.rstrip())
     lineNum=lineNum+1
