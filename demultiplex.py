5e2bc9ee6b2d86753e9dbc2f1ac43516dea16b69
Ariel Levi Simons <levisimons@gmail.com>
2017-06-17 12:19:53 -0700

    Update demultiplex.py

--- demultiplex.py
+++ demultiplex.py
@@ -4,11 +4,15 @@ from itertools import izip
 
 lineNum=1
 
+leftIndexList = ['TCAGC','GTATC','GCTAC','ACGCA',',CGCGT','CTGGT','GCGTT','GGAAC','AAGCC','AGCTT','CCCTT','CGCAC','GGTGT',
+                 'GGCAG','TGATA','TGTGC']
+
 leftRead = gzip.open('Undetermined_S0_L001_R1_001.fastq.gz','r')
 rightRead = gzip.open('Undetermined_S0_L001_R2_001.fastq.gz','r')
 rightIndex = gzip.open('Undetermined_S0_L001_I1_001.fastq.gz','r')
 for leftLine, rightLine, indexLine in izip(leftRead, rightRead, rightIndex):
     if lineNum%4 == 2:
         leftIndex=leftLine[4:9]
-        print (leftIndex.rstrip(), leftLine.rstrip(), rightLine.rstrip(), indexLine.rstrip())
+        if leftIndex in leftIndexList: primerNum = leftIndexList.index(leftIndex)
+        print (leftIndex.rstrip(), primerNum, leftLine.rstrip(), rightLine.rstrip(), indexLine.rstrip())
     lineNum=lineNum+1
