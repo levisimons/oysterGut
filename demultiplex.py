caef0fdbdee9af2d634dc33f06744e0e16d5dacb
Ariel Levi Simons <levisimons@gmail.com>
2017-06-17 11:02:14 -0700

    Update demultiplex.py

--- demultiplex.py
+++ demultiplex.py
@@ -5,5 +5,5 @@ from itertools import izip
 leftRead = gzip.open('Undetermined_S0_L001_R1_001.fastq.gz','r')
 rightRead = gzip.open('Undetermined_S0_L001_R2_001.fastq.gz','r')
 rightIndex = gzip.open('Undetermined_S0_L001_I2_001.fastq.gz','r')
-for leftRead, rightRead, rightIndex in izip(leftRead, rightRead, rightIndex):
-    print "%s\t%s" % (leftRead.rstrip(), rightRead.rstrip(), rightIndex.rstrip())
+for leftLine, rightLine, indexLine in izip(leftRead, rightRead, rightIndex):
+    print "%s\t%s" % (leftLine.rstrip(), rightLine.rstrip(), indexLine.rstrip())
