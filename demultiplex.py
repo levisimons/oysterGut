e65e136c8c3744ffee342a956b3b95e535ba2075
Ariel Levi Simons <levisimons@gmail.com>
2017-06-17 09:57:38 -0700

    Update demultiplex.py

--- demultiplex.py
+++ demultiplex.py
@@ -1,6 +1,6 @@
 import os,re,sys
 import gzip
 
-rightIndex=gzip.open('Undetermined_S0_L001_R1_001.fastq.gz','rb')
-rightIndexRead=f.read()
-print rightIndexRead
+with gzip.open('Undetermined_S0_L001_R1_001.fastq.gz','r') as rightIndex:        
+            for line in rightIndex:        
+                print('got line', line)
