15325dca4d26b7fad7d9d0022caf13e757f1b0b6
Ariel Levi Simons <levisimons@gmail.com>
2017-06-27 11:59:05 -0700

    Update sifter.py

--- sifter.py
+++ sifter.py
@@ -1,8 +1,15 @@
 import os,re,sys
-import subprocess
+from subprocess import call
 
-inputFile = open('unused.txt','r')
+inputFile = open('unused.txt','rU')
 
 for line in inputFile:
-  print line
-  os.system(mv line'*.fastq unused')
+  line=line.rstrip('\r\n')
+  command1 = 'mv ',line,'_L001_R1_001.fastq unused'
+  command1 = ''.join(command1)
+  command2 = 'mv ',line,'_L001_R2_001.fastq unused'
+  command2 = ''.join(command2)
+  print command1
+  print command2
+  call(command1)
+  call(command2)
