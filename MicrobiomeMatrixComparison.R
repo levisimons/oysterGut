93caa624558b9ee9dace21bd618c7b0d04671bfa
Ariel Levi Simons <levisimons@gmail.com>
2017-08-22 16:34:57 -0700

    Update MicrobiomeMatrixComparison.R

--- MicrobiomeMatrixComparison.R
+++ MicrobiomeMatrixComparison.R
@@ -21,7 +21,7 @@ setwd("~/Desktop/OysterGut/Analysis")
 ## Column 3 is the correlation between the abundances of those two OTUS are observed across a set of samples.
 ## Column 4 is the p-value of the correlation.
 ## If the p-value of a particular correlation is above 0.05 then it is disregarded.
-M1 <- read.table("stability.CHAEFecal.0.03.Top30.pearson.otu.corr", header = TRUE, sep="\t", as.is=T)
+M1 <- read.table("stability.ISOFeed.0.03.Top30.pearson.otu.corr", header = TRUE, sep="\t", as.is=T)
 M1 <- ifelse(M1$Significance>0.05,NA,M1$pearsonCoef)
 
 ## Read in the second .corr file.
@@ -30,8 +30,8 @@ M1 <- ifelse(M1$Significance>0.05,NA,M1$pearsonCoef)
 ## Column 3 is the correlation between the abundances of those two OTUS are observed across a set of samples.
 ## Column 4 is the p-value of the correlation.
 ## If the p-value of a particular correlation is above 0.05 then it is disregarded.
-M2 <- read.table("stability.CHAEGut.0.03.Top30.pearson.otu.corr", header = TRUE, sep="\t", as.is=T)
+M2 <- read.table("stability.TETFecalControl.0.03.Top30.pearson.otu.corr", header = TRUE, sep="\t", as.is=T)
 M2 <- ifelse(M2$Significance>0.05,NA,M2$pearsonCoef)
 
 ## Compute the t-statistical test comparing the two correlation vectors
-cor.test(M1,M2)
+cor.test(M1,M2,na.action=na.omit)
