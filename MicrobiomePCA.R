82d90532651a680bc5dfc293143ec8162d913792
Ariel Levi Simons <levisimons@gmail.com>
2017-09-05 15:12:51 -0700

    Update MicrobiomePCA.R

--- MicrobiomePCA.R
+++ MicrobiomePCA.R
@@ -2,10 +2,26 @@ library(ggplot2)
 library(ggfortify)
 library(rgl)
 
-setwd("~/Desktop/OysterGut/Analysis")
+setwd("~/Desktop/OysterMicrobiome")
 
-## Read in relative abundance per OTUS data for oyster microbiome study samples
-RabundData <- read.table("oystersRabund", header = TRUE, sep="\t", as.is=T)
+## Read in data representing relative abundance values for each OTU by oyster sample.
+## The absolute sequence counts by OTU are normalized by total sequences per sample.
+## The data and design factors here are for the oyster microbiome samples.
+oyster <- read.table("oyster", header = TRUE, sep="\t", as.is=T)
+oysterAbundances <- read.table("oysterAbundances", header = TRUE, sep="\t", as.is=T)
+
+## Read in data representing relative abundance values for each OTU by oyster sample.
+## The absolute sequence counts by OTU are normalized by total sequences per sample.
+## The data and design factors here are for the feedstock microbiome samples.
+feed <- read.table("feedstock", header = TRUE, sep="\t", as.is=T)
+feedAbundances <- read.table("feedAbundances", header = TRUE, sep="\t", as.is=T)
+
+## Merge the experimental design factors with the relative OTU abundance data.
+## Choose either the first or second merge to choose either the feedstock
+## or oyster microbiome data set to graph.
+RabundData <- merge(feed,feedAbundances,all.Group=TRUE)
+RabundData <- merge(oyster,oysterAbundances,all.Group=TRUE)
+
 ## Ignore metadata columns.
 ## For the fecal pellet and stomach samples this is columns 1 through 8.
 ## For the feedstock samples this is columns 1 through 5.
@@ -25,8 +41,8 @@ autoplot(prcomp(RabundOTUs), data=RabundData,
          ylab=paste('PCA2, percent variation: ',round(var.percent[2], digits=3)),
          main='PCA2 vs. PCA1 Oyster stomach and fecal pellet communities',
          loadings=FALSE, loadings.label=FALSE, 
-         shape=TRUE, label=FALSE, loadings.label.size = 3, colour='FeedType')
-plot3d(pc$scores[,1:3], col=iris$Species)
+         shape=TRUE, label=FALSE, loadings.label.size = 3, colour='ExperimentalStatus')
+
 summary(prcomp(RabundOTUs))
 
 head(unclass(RabundOTUs.pca$rotation)[,1:4])
@@ -35,7 +51,11 @@ RabundOTUsPC <- princomp(RabundOTUs[,1:4])
 ## Convert feed type from a name to a numerical scale for later plotting.
 FeedNum <- as.numeric(as.factor(RabundData$FeedType))
 ## Plot the three largest principal components.
-plot3d(RabundOTUsPC$scores[,1:3], col=FeedNum)
+plot3d(RabundOTUsPC$scores[,1:3],
+       xlab=paste('PCA1, percent variation: ',round(var.percent[1], digits=3)),
+       ylab=paste('PCA2, percent variation: ',round(var.percent[2], digits=3)),
+       zlab=paste('PCA3, percent variation: ',round(var.percent[3], digits=3)),
+       col=FeedNum)
 ## Include a legend for the scale for feed type.
 legend3d("topleft", col=1:3, legend = c("CHAE","ISO","TET"), pch = 20, bty='n', cex=.75)
 summary(RabundOTUsPC)
