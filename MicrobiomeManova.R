2f2c98dd27c2b3976115f07fb5b92995b9f7de7f
Ariel Levi Simons <levisimons@gmail.com>
2017-09-05 14:25:58 -0700

    Update MicrobiomeManova.R

--- MicrobiomeManova.R
+++ MicrobiomeManova.R
@@ -14,18 +14,21 @@ library(heatmap3)
 ## Read in data
 ## There is a separate feedstock and oyster file.
 ## The first columns contain important experimental information.
-setwd("~/Desktop/OysterGut/Analysis")
+setwd("~/Desktop/OysterGut/OysterMicrobiome")
 
 ## Read in data representing relative abundance values for each OTU by oyster sample.
 ## The absolute sequence counts by OTU are normalized by total sequences per sample.
-oysters <- read.table("oystersRabund", header = TRUE, sep="\t", as.is=T)
+oyster <- read.table("oyster", header = TRUE, sep="\t", as.is=T)
+oysterAbundances <- read.table("oysterAbundances", header = TRUE, sep="\t", as.is=T)
+
+## Merge the experimental design factors with the relative OTU abundance data.
+oysters <- merge(oyster,oysterAbundances,all.Group=TRUE)
 
 ## Ignore the oyster gut community sample rows.
 oysters <- oysters[-c(6,22,31,48,64,74,86,93),]
 
 ## Treat the relative abundances of the selected OTUs as a variable.
-TopOysterOTUs <- as.matrix(oysters[,c(9:18)])
-Marimononas <- as.matrix(oysters[,c(72,3267,4023)])
+TopOysterOTUs <- as.matrix(oysters[,c(9:19)])
 
 ## Test the signifance of experimental variables on the selected OTU data.
 OysterManova <- manova(TopOysterOTUs ~ OysterID*FeedType, data=oysters)
@@ -33,7 +36,11 @@ summary(OysterManova)
 
 ## Read in data representing relative abundance values for each OTU by feedstock sample.
 ## The absolute sequence counts by OTU are normalized by total sequences per sample.
-feed <- read.table("feedRabund", header = TRUE, sep="\t", as.is=T)
+feedstock <- read.table("feedstock", header = TRUE, sep="\t", as.is=T)
+feedAbundances <- read.table("feedAbundances", header = TRUE, sep="\t", as.is=T)
+
+## Merge the experimental design factors with the relative OTU abundance data.
+feed <- merge(feedstock,feedAbundances,all.Group=TRUE)
 
 ## Treat the relative abundances of the selected OTUs as a variable.
 TopFeedOTUs <- as.matrix(feed[,c(6:15)])
