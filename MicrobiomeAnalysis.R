37a252ba8f297ded91368568a12c2aedb4828a2e
Ariel Levi Simons <levisimons@gmail.com>
2017-09-11 16:06:54 -0700

    Update MicrobiomeAnalysis.R

--- MicrobiomeAnalysis.R
+++ MicrobiomeAnalysis.R
@@ -11,6 +11,8 @@ OTUCount <- import_mothur(mothur_shared_file = "stability.opti_mcc.shared",
                           mothur_constaxonomy_file = "stability.cons.taxonomy")
 ## Import phylogenetic tree into microbiome data set.
 phy_tree(OTUCount) <- rtree(ntaxa(OTUCount), rooted=TRUE, tip.label=taxa_names(OTUCount))
+## Determine a sequence count by sample threshold for subsampling.
+OTUThreshold = as.integer(1e-4*sum(sample_sums(OTUCount)))
 
 ## Read in experimental design variables, such as the week the sample was taken.
 factors <- read.table("MicrobiomeFactors.csv", header=TRUE, sep=",",as.is=T)
@@ -21,26 +23,25 @@ microbiomeRaw <- merge_phyloseq(OTUCount,factors)
 ## Update the taxonomic order names.
 colnames(tax_table(microbiomeRaw)) <- c("kingdom", "phylum", "class", "order", "family",  "genus")
 
+## Filter out samples which contain less than a certain fraction of the total
+## sequence count.  This is used to help make sampling uniform across samples.
+microbiomeRaw = prune_samples(sample_sums(microbiomeRaw) > OTUThreshold, microbiomeRaw )
+set.seed(42)
+microbiomeRaw<-rarefy_even_depth(microbiomeRaw, sample.size = 1e-4*sum(sample_sums(microbiomeRaw)),rngseed = FALSE, replace = TRUE, trimOTUs = TRUE, verbose = TRUE)
 
 ## If you want to scale OTU by relative sequence abundance.
-OTUCount = transform_sample_counts(OTUCount,function(x) x / sum(x))
+## This is needed for any beta diversity analysis.
+## OTUs with a rarity below a given cutoff are removed from this analysis.
+microbiome = transform_sample_counts(microbiomeRaw,function(x) x / sum(x))
 ## If you want to filter out OTUs with a relative abundance below a given cutoff.
-OTUCount = filter_taxa(OTUCount, function(x) mean(x) > 1e-2, TRUE)
-## Read in experimental design variables, such as the week the sample was taken.
-factors <- read.table("MicrobiomeFactors.csv", header=TRUE, sep=",",as.is=T)
-## Convert experimental design variables into a dataframe.
-factors <- sample_data(data.frame(factors, row.names=sample_names(OTUCount)))
-## Merge relative OTU abundance data with design variables into a Phyloseq object.
-microbiome <- merge_phyloseq(OTUCount,factors)
-## Update the taxonomic order names.
-colnames(tax_table(microbiome)) <- c("kingdom", "phylum", "class", "order", "family",  "genus")
+microbiome = filter_taxa(microbiomeRaw, function(x) mean(x) > 1e-3, TRUE)
 
 ## Load distance methods.
 dist_methods <- unlist(distanceMethodList)
 print(dist_methods)
 ## Remove the user-defined distances.
 ## Choose 2 for weight unifrac and 8 for bray-curtis.
-dist_methods <- dist_methods[c(2,8)]
+dist_methods <- dist_methods[c(2)]
 print(dist_methods)
 
 ## Loop through each distance method, save each plot to a list, called plist.
@@ -49,8 +50,6 @@ names(plist) = dist_methods
 for( i in dist_methods ){
   # Calculate distance matrix
   iDist <- distance(microbiome, method=i)
-  x <- cmdscale(iDist,eig=TRUE)
-  print(x$eig[1:4])
   # Calculate ordination
   iMDS  <- ordinate(microbiome, "MDS", distance=iDist)
   ## Make plot
@@ -63,12 +62,13 @@ for( i in dist_methods ){
   # Save the graphic to file.
   plist[[i]] = p
 }
-
-## If you want to plot the distance metrics in a grid of plots using available data.
+p
+## If you want to plot the distance metrics
+## in a grid of plots using available data.
 df = ldply(plist, function(x) x$data)
 names(df)[1] <- "distance"
 p = ggplot(df, aes(Axis.1, Axis.2, color=SampleType))
-p = p + geom_point(size=3, alpha=0.5)
+p = p + geom_point(size=1.5, alpha=0.5)
 p = p + facet_wrap(~distance, scales="free")
 p = p + ggtitle("Multidimensional Distance Scalings \n various distance metrics for oyster microbiome data")
 p
@@ -76,6 +76,8 @@ p
 ## Plot Shannon index of samples.  Color by a design variable.
 plot_richness(microbiomeRaw, measures=c("Shannon","Chao1"),color="SampleType",title="Alpha diversity metrics \n oyster microbiome data")
 
+## Store alpha diversity metrics, concatenated with experimental variables,
+## in a single dataframe for subsequent significance tests.
 alphaDiversity <- estimate_richness(microbiomeRaw,measures=c("Shannon","Chao1"))
 alphaDiversity <- as.data.frame(alphaDiversity)
 alphaDiversity$Group <- rownames(alphaDiversity)
@@ -85,6 +87,18 @@ alphaDiversity <- merge(factors,alphaDiversity,by=0)
 alphaDiversity <- subset(alphaDiversity, select=-c(1,8))
 colnames(alphaDiversity)[colnames(alphaDiversity)=="Group.x"] <- "Group"
 alphaDiversity <- as.data.frame(alphaDiversity)
-a <- alphaDiversity$Shannon[alphaDiversity$SampleType=="GUT"]
-b <- alphaDiversity$Shannon[alphaDiversity$SampleType=="FEED"]
+
+## Store alpha diversity metrics and select them by value for a particular design variable.
+a <- alphaDiversity$Shannon[alphaDiversity$SampleType=="FEED"]
+b <- alphaDiversity$Shannon[alphaDiversity$SampleType!="FEED"]
+
+## Perform a Wilcoxon statistical test of significance on
+## alpha diversity metrics separated by a design variable.
 wilcox.test(a,b)
+
+## Perform a PERMANOVA using a set number of permutations on a particular
+## beta diversity metric and the significance of a particular design variable.
+microbiomeDF = as(sample_data(microbiome), "data.frame")
+microbiomeDist = distance(microbiome,method="wunifrac")
+microbiomeAdonis = adonis(microbiomeDist ~ FeedType, microbiomeDF,permutations = 10000)
+microbiomeAdonis
