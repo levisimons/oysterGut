d2fce0c21575933672cfb37b4c21af7e753f85c1
Ariel Levi Simons <levisimons@gmail.com>
2017-09-13 15:13:52 -0700

    Update MicrobiomeAnalysis.R

--- MicrobiomeAnalysis.R
+++ MicrobiomeAnalysis.R
@@ -17,6 +17,10 @@ OTUThreshold = as.integer(1e-4*sum(sample_sums(OTUCount)))
 ## Read in experimental design variables, such as the week the sample was taken.
 factors <- read.table("MicrobiomeFactors.csv", header=TRUE, sep=",",as.is=T)
 ## Convert experimental design variables into a dataframe.
+## The design variables for the oyster microbiome study are:
+## FeedType (TET, ISO, or CHAE), SampleType (FEED, GUT, or FECAL)
+## WeekFromStart (1-12), SampleID (1-5, TETFeed, ISOFeed, and CHAEFeed)
+## Group (All sample group names), ExperimentalStatus (EXP or CON)
 factors <- sample_data(data.frame(factors, row.names=sample_names(OTUCount)))
 ## Merge relative OTU abundance data with design variables into a Phyloseq object.
 microbiomeRaw <- merge_phyloseq(OTUCount,factors)
@@ -57,16 +61,17 @@ for( i in dist_methods ){
   # Don't carry over previous plot (if error, p will be blank)
   p <- NULL
   # Create plot, store as temp variable, p
-  p <- plot_ordination(microbiome, iMDS, color="SampleType")
+  p <- plot_ordination(microbiome, iMDS, color="FeedType")
   # Add title to each plot
   p <- p + ggtitle(paste("MDS using distance method ", i, sep=""))
   # Save the graphic to file.
   plist[[i]] = p
 }
-p
+p + geom_point(size=3.5, alpha=1)
+
 
 ## Plot Shannon index of samples.  Color by a design variable.
-plot_richness(microbiomeRaw, measures=c("Shannon","Observed"),color="FeedType",title="Alpha diversity metrics \n oyster microbiome data")
+plot_richness(microbiomeRaw, measures=c("Shannon","Observed"),color="WeekFromStart",title="Alpha diversity metrics \n oyster microbiome data")
 
 ## Store alpha diversity metrics, concatenated with experimental variables,
 ## in a single dataframe for subsequent significance tests.
@@ -81,8 +86,12 @@ colnames(alphaDiversity)[colnames(alphaDiversity)=="Group.x"] <- "Group"
 alphaDiversity <- as.data.frame(alphaDiversity)
 
 ## Store alpha diversity metrics and select them by value for a particular design variable.
-a <- alphaDiversity$Shannon[alphaDiversity$FeedType=="ISO"]
-b <- alphaDiversity$Shannon[alphaDiversity$FeedType=="CHAE"]
+a <- alphaDiversity$Shannon[alphaDiversity$WeekFromStart<=3]
+b <- alphaDiversity$Shannon[alphaDiversity$WeekFromStart>=3]
+
+## Plot alpha diversity metrics versus time
+plot(alphaDiversity$WeekFromStart,alphaDiversity$Shannon, xlab="Week", ylab="Sample Shannon index",main="Sample alpha diversity by week")
+plot(alphaDiversity$WeekFromStart,alphaDiversity$Observed, xlab="Week", ylab="Sample OTU richnes",main="Sample OTU richness by week")
 
 ## Perform a Wilcoxon statistical test of significance on
 ## alpha diversity metrics separated by a design variable.
@@ -122,9 +131,9 @@ colnames(tax_table(microbiomeRaw)) <- c("kingdom", "phylum", "class", "order", "
 
 ## Find the most abundant OTUs as selected by an experiment variable
 ## such as feed type.
-aTaxa = 5
-algae = "ISO"
-MBSubset = subset_samples(microbiomeRaw,FeedType==algae)
+aTaxa = 10
+algae = "all"
+MBSubset = subset_samples(microbiomeRaw)
 MBAbundant = sort(taxa_sums(microbiomeRaw), TRUE)[1:aTaxa]
 MBSubset = prune_taxa(names(MBAbundant), MBSubset)
 MBSubset = transform_sample_counts(MBSubset,function(x) x / sum(x))
@@ -132,6 +141,6 @@ MBSubset = transform_sample_counts(MBSubset,function(x) x / sum(x))
 ## Plot the most abundant OTUs
 b = plot_bar(MBSubset, fill="genus", facet_grid = ~SampleType)
 b = b + geom_bar(aes(color=genus, fill=genus), stat="identity", position="stack")
-b = b + ggtitle(paste("Most abundant",aTaxa,"OTUs by sample for the",algae,"feedstock",sep=" "))
+b = b + ggtitle(paste("Most abundant",aTaxa,"OTUs by sample for",algae,"feedstock",sep=" "))
 b = b + labs(y = "Relative sequence abundance")
 b
